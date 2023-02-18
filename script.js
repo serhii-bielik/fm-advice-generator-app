5a0903650fccea334fd8f41f38d0327ab0a1db1d
Serhii Bielik <bielik.sergey@gmail.com>
2023-02-18 19:58:44 +0200

    add change advice animation

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const QUOTES_API_URL = 'https://api.adviceslip.com/advice';
 
 const quoteId = document.querySelector('.advice__id > span');
 const quoteContent = document.querySelector('.advice__content > span');
+const adviceArea = document.querySelector('#advice__area');
 
 async function getRandomQuote() {
   try {
-    const response = await fetch(QUOTES_API_URL, {cache: "no-store"});
+    const response = await fetch(QUOTES_API_URL, { cache: 'no-store' });
     const quoteData = await response.json();
     return quoteData.slip;
   } catch (error) {
@@ -14,9 +15,13 @@ async function getRandomQuote() {
 }
 
 function updateQuote() {
-  getRandomQuote().then((quoteData) => {    
-    quoteId.innerText = quoteData.id;    
-    quoteContent.innerText = quoteData.advice;
+  getRandomQuote().then((quoteData) => {
+    adviceArea.style.opacity = 0;
+    setTimeout(function () {
+      quoteId.innerText = quoteData.id;
+      quoteContent.innerText = quoteData.advice;
+      adviceArea.style.opacity = 1;
+    }, 500);
   });
 }
 
